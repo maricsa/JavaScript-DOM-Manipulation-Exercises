2a7667f2ce5957da23f37b1183ddfda8d2486b47
Jian-Lei Hu <whosjianlei@gmail.com>
2016-04-13 10:44:05 -0400

    finished username and password

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ $(document).ready(function(){
 
 
 
-$('button').click(function(){
-	var x = $('input').val();
+$('#bdayb').click(function(){
+	var x = $('#bday').val();
 	if (x.length !== 10){
 		alert("Please put in your Birthday!");
+	} else {
+		console.log(x)
+	}
+});
+
+$('#usernameb').click(function(){
+	var y = $('#username').val();
+	if (y.length < 1 || y.length > 10){
+		alert("Please create a username 10 characters or less!");
+	} else {
+		console.log(y)
+	}
+});
+
+$('#passwordb').click(function(){
+	var z = $('#password').val();
+	if (z !== "12345"){
+		alert("Password does not match!");
+	} else {
+		console.log(z)
 	}
 });
 
